d048470adb557245bf80e3207c0e91b03a0ebca2
Florian DEMATRAZ <florian.dematraz@egerie.eu>
2023-06-08 11:42:17 +0200

    Completely removed old powerline

--- configure.sh
+++ configure.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export GOPATH=$HOME/go
 export PATH=$PATH:/usr/local/go/bin
 
-sudo apt-get install powerline yamllint shellcheck
+sudo apt-get install -y yamllint shellcheck
 
 mkdir -p "$HOME/.tmux/plugins"
 ln -s -f "$SCRIPT_DIR/tmux/tpm" "$HOME/.tmux/plugins/tpm"
@@ -14,14 +14,6 @@ ln -s -f "$SCRIPT_DIR/tmux/tmux.conf.local" "$HOME/.tmux.conf.local"
 curl -o go-latest.tar.gz https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go-latest.tar.gz
 go install github.com/justjanne/powerline-go@lates
-
-#mkdir -p "$HOME/.config/powerline/"
-#ln -s -f "$SCRIPT_DIR/powerline/powerline/config_files/colorschemes" "$HOME/.config/powerline/colorschemes"
-#ln -s -f "$SCRIPT_DIR/powerline/powerline/config_files/themes" "$HOME/.config/powerline/themes"
-#ln -s -f "$SCRIPT_DIR/powerline/powerline/config_files/colors.json" "$HOME/.config/powerline/colors.json"
-#ln -s -f "$SCRIPT_DIR/powerline_config.json" "$HOME/.config/powerline/config.json"
-#ln -s -f "$SCRIPT_DIR/powerline_shell_theme.json" "$HOME/.config/powerline/themes/shell/powerline_shell_theme.json"
-#ln -s -f "$SCRIPT_DIR/powerline_shell_colorscheme.json" "$HOME/.config/powerline/colorschemes/shell/powerline_shell_colorscheme.json"
 
 mkdir -p "$HOME/.vim/pack/dist/start"
 ln -s -f "$SCRIPT_DIR/vim/vim-airline" "$HOME/.vim/pack/dist/start/vim-airline"
