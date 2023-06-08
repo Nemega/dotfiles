0f59dfa32b5c64e93f9094646b971cbc2eea5818
Florian Dematraz <florian.dematraz@egerie.eu>
2023-06-08 10:40:42 +0200

    new powerline

--- configure.sh
+++ configure.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export GOPATH=$HOME/go
+export PATH=$PATH:/usr/local/go/bin
 
 sudo apt-get install powerline yamllint shellcheck
 
@@ -9,13 +11,17 @@ ln -s -f "$SCRIPT_DIR/tpm" "$HOME/.tmux/plugins/tpm"
 ln -s -f "$SCRIPT_DIR/tmux/.tmux.conf" "$HOME/.tmux.conf"
 ln -s -f "$SCRIPT_DIR/tmux.conf.local" "$HOME/.tmux.conf.local"
 
-mkdir -p "$HOME/.config/powerline/"
-ln -s -f "$SCRIPT_DIR/powerline/powerline/config_files/colorschemes" "$HOME/.config/powerline/colorschemes"
-ln -s -f "$SCRIPT_DIR/powerline/powerline/config_files/themes" "$HOME/.config/powerline/themes"
-ln -s -f "$SCRIPT_DIR/powerline/powerline/config_files/colors.json" "$HOME/.config/powerline/colors.json"
-ln -s -f "$SCRIPT_DIR/powerline_config.json" "$HOME/.config/powerline/config.json"
-ln -s -f "$SCRIPT_DIR/powerline_shell_theme.json" "$HOME/.config/powerline/themes/shell/powerline_shell_theme.json"
-ln -s -f "$SCRIPT_DIR/powerline_shell_colorscheme.json" "$HOME/.config/powerline/colorschemes/shell/powerline_shell_colorscheme.json"
+curl -o go-latest.tar.gz https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go-latest.tar.gz
+go install github.com/justjanne/powerline-go@lates
+
+#mkdir -p "$HOME/.config/powerline/"
+#ln -s -f "$SCRIPT_DIR/powerline/powerline/config_files/colorschemes" "$HOME/.config/powerline/colorschemes"
+#ln -s -f "$SCRIPT_DIR/powerline/powerline/config_files/themes" "$HOME/.config/powerline/themes"
+#ln -s -f "$SCRIPT_DIR/powerline/powerline/config_files/colors.json" "$HOME/.config/powerline/colors.json"
+#ln -s -f "$SCRIPT_DIR/powerline_config.json" "$HOME/.config/powerline/config.json"
+#ln -s -f "$SCRIPT_DIR/powerline_shell_theme.json" "$HOME/.config/powerline/themes/shell/powerline_shell_theme.json"
+#ln -s -f "$SCRIPT_DIR/powerline_shell_colorscheme.json" "$HOME/.config/powerline/colorschemes/shell/powerline_shell_colorscheme.json"
 
 mkdir -p "$HOME/.vim/pack/dist/start"
 ln -s -f "$SCRIPT_DIR/vim-airline" "$HOME/.vim/pack/dist/start/vim-airline"
@@ -23,3 +29,5 @@ ln -s -f "$SCRIPT_DIR/vim-airline-themes" "$HOME/.vim/pack/dist/start/vim-airlin
 ln -s -f "$SCRIPT_DIR/vim-colors-solarized" "$HOME/.vim/pack/dist/start/vim-colors-solarized"
 ln -s -f "$SCRIPT_DIR/syntastic" "$HOME/.vim/pack/dist/start/syntastic"
 ln -s -f "$SCRIPT_DIR/vimrc" "$HOME/.vimrc"
+
+ln -s -f "$SCRIPT_DIR/bashrc" "$HOME/.bashrc"
