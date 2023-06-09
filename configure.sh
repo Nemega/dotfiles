d563d3cf2eb7d25cf00f384f065ee976fef64313
Florian Dematraz <florian.dematraz@egerie.eu>
2023-06-09 15:22:43 +0200

    Solarized prompt

--- configure.sh
+++ configure.sh
@@ -14,6 +14,8 @@ ln -s -f "$SCRIPT_DIR/tmux/tmux.conf.local" "$HOME/.tmux.conf.local"
 curl -o go-latest.tar.gz https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go-latest.tar.gz
 go install github.com/justjanne/powerline-go@lates
+mkdir -p "$GOPATH/powerline-go"
+ln -s -f "$SCRIPT_DIR/bash/solarized_theme.json" "$GOPATH/powerline-go/solarized_theme.json"
 
 mkdir -p "$HOME/.vim/pack/dist/start"
 ln -s -f "$SCRIPT_DIR/vim/vim-airline" "$HOME/.vim/pack/dist/start/vim-airline"
@@ -22,4 +24,4 @@ ln -s -f "$SCRIPT_DIR/vim/vim-colors-solarized" "$HOME/.vim/pack/dist/start/vim-
 ln -s -f "$SCRIPT_DIR/vim/syntastic" "$HOME/.vim/pack/dist/start/syntastic"
 ln -s -f "$SCRIPT_DIR/vim/vimrc" "$HOME/.vimrc"
 
-ln -s -f "$SCRIPT_DIR/bashrc" "$HOME/.bashrc"
+ln -s -f "$SCRIPT_DIR/bash/bashrc" "$HOME/.bashrc"
